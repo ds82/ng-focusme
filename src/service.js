3c462f59f7240a38aca1d826456b0117b6874c3a
Dennis Sänger <mail@dennis.io>
2015-05-05 15:09:11 +0200

    fix(focus): add $timeout to element.focus()
    This fixes some situations where an immediate call to .focus() would fail

--- src/service.js
+++ src/service.js
@@ -3,7 +3,8 @@
 angular.module('io.dennis.focusme')
   .service('FocusMe', FocusMe);
 
-function FocusMe() {
+FocusMe.$inject = ['$timeout'];
+function FocusMe($timeout) {
   var pub = this;
   var map = {};
 
@@ -19,8 +20,16 @@ function FocusMe() {
 
   function focus(key) {
 
-    if (map[key]) {
-      map[key][0].focus();
+    var element = map[key];
+    if (angular.isDefined(element)) {
+      $timeout(focusElement);
+    }
+
+    function focusElement() {
+      element = (angular.isElement(element)) ?
+        element : element[0];
+      element.focus();
     }
   }
+
 }
